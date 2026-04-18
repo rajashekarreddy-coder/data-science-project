e6661516749b89bed11b9b3aa3fce2718f31fc02
Rajashekar reddy <vangalarajashekarreddy6@gmail.com>
2026-04-19 01:01:13 +0530

    Add student grades classification based on marks

---
+++ student_grades.py
@@ -0,0 +1,9 @@
+marks = [85, 70, 90, 60, 88]
+
+for mark in marks:
+    if mark >= 80:
+        print(mark, "Grade A")
+    elif mark >= 60:
+        print(mark, "Grade B")
+    else:
+        print(mark, "Grade C")
