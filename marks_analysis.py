00c03af2b2c6a0ed37b84ed86f4ae727c9084326
Rajashekar reddy <vangalarajashekarreddy6@gmail.com>
2026-04-19 00:59:46 +0530

    Add marks analysis script to calculate statistics

---
+++ marks_analysis.py
@@ -0,0 +1,8 @@
+marks = [85, 90, 78, 92, 88]
+
+average = sum(marks) / len(marks)
+
+print("Student Marks:", marks)
+print("Average Marks:", average)
+print("Highest Marks:", max(marks))
+print("Lowest Marks:", min(marks))
